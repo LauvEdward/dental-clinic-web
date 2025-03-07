dfc23b2585d851cc44abed9da3999a7c530c872c
Le Dinh Phuc <phucld@ominext.com>
2025-03-07 11:38:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="app-title">Nha Khoa Cám User</h1>
+      <p className="app-description">
+        Chào mừng bạn đến với hệ thống quản lý nha khoa. Hãy đăng nhập để bắt đầu làm việc.
+      </p>
     </div>
   );
 }
